d08beef0dc851ff0cdaaf320c98495b4669fee0a
pavly <pr253@njit.edu>
2022-09-10 17:38:43 -0400

    finish

---
+++ app/src/main/java/com/example/todoapp/NoteItemAdaptor.kt
@@ -0,0 +1,42 @@
+package com.example.todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskItemAdapter(val listOfNotes: List<String>, val longClickListener: OnLongClickListener) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+    interface OnLongClickListener {
+        fun onItemLongClicked(position: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listOfNotes.get(position)
+        holder.textView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listOfNotes.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView
+
+        init {
+            textView = itemView.findViewById(android.R.id.text1)
+            itemView.setOnLongClickListener {
+                longClickListener.onItemLongClicked(adapterPosition)
+                true
+            }
+        }
+    }
+}
